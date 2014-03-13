6ff96c7495b417019843f8670e30ade8ac21b011
Junjun Guo <guojunjun@gmail.com>
2014-03-13 17:06:35 +0100

    Create client.py

---
+++ client.py
@@ -0,0 +1,47 @@
+'''
+KTN-project 2013 / 2014
+'''
+import socket
+from MessageWorker import *
+from time import gmtime, strftime
+class Client(object):
+
+    def __init__(self):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def start(self, host, port):
+        self.connection.connect((host, port))
+        messageThread = ReceiveMessageWorker(client, self.connection)
+        messageThread.daemeon = True
+        messageThread.start()
+        print "MessageWorker:",messageThread.name
+
+    def message_received(self,message,connection):
+        #self.connection.recv(1024).strip()
+        print message+" M "
+
+    def connection_closed(self, connection):
+        connection.close()
+        print "Connection: "+connection+" closed!"
+
+    def send(self, data):
+        self.connection.sendall(data)
+
+    def force_disconnect(self):
+        self.connection.close()
+        print "connection closed!"
+
+name = "Ole"
+
+if __name__ == "__main__":
+    client = Client()
+    client.start('localhost', 9988)
+    on = True
+    while(on):
+        r = raw_input(name+': ')
+        if r == 'logut':
+            on = False
+        tid = strftime("%a, %d %b %Y %H:%M:%S", gmtime())
+        if r:
+            client.send(name+' said @ '+tid+' : '+r)
+client.force_disconnect()        
