42390c32e0000e416dad0e3204076f8403f2ce69
Junjun Guo <guojunjun@gmail.com>
2014-03-14 15:40:06 +0100

    Update server.py

--- server.py
+++ server.py
@@ -4,6 +4,12 @@ Very simple server implementation that should serve as a basis
 for implementing the chat server
 '''
 import SocketServer
+#import json
+
+# client list:
+clients = []
+#message stack
+messagestack = []
 
 '''
 The RequestHandler class for our server.
@@ -12,6 +18,9 @@ override the handle() method to implement communication to the
 client.
 '''
 class CLientHandler(SocketServer.BaseRequestHandler):
+    #def login(self, json_handle):
+       # user_name = json_handle.get('user_name')
+       # pattern = 
     def handle(self):
         #Get a reference to the socket object
         self.connection = self.request
@@ -20,12 +29,12 @@ class CLientHandler(SocketServer.BaseRequestHandler):
         # Get the remote port number of the socket
         self.port = self.client_address[1]
         print 'Client connected @' + self.ip + ':' + str(self.port)
+        clients.append(self)
         # Wait for data from the client
         on = True
         while (on):
             # Check if the data exists
             data = self.connection.recv(1024).strip()
-            self.server.gmessage.append(data)
             # (recv could have returned due to a disconnect)
             logut = data[-5:]
             if logut == 'logut':
@@ -35,30 +44,21 @@ class CLientHandler(SocketServer.BaseRequestHandler):
             else:
                 if data:
                     print data 
-                    # Return the string in uppercase
-                    self.send(data)
-                    #self.connection.sendall(gmessage.upper())
-                   # gmessage = null
+                    messagestack.append(data)
                 else:
                     print 'Client disconnected!'
                     on = False
-            for message in gmessage:
-                self.server.broadcast(message)
-                print message
+            for client in clients:
+                client.send(data)
     def send (self, data):
         self.connection.sendall(data.upper())
+ 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     allow_reuse_address = True
-    gmessage = []
-    clients = {}
-    def broadcast(self, message):
-        for client in self.clients:
-            client.sendall(message)
 
 if __name__ == "__main__":
     HOST = 'localhost'
     PORT = 9988
-    gmessage = 0
     # Create the server, binding to localhost on port 9999
     server = ThreadedTCPServer((HOST, PORT), CLientHandler)
     # Activate the server; this will keep running until you
